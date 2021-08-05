2dbf2994ca28a8af83527217390ed23d97470044
lmachens <leon@machens.koeln>
2021-08-05 15:51:26 +0200

    💄 Ease in and out menu

--- src/app/components/MapFilter/MapFilter.tsx
+++ src/app/components/MapFilter/MapFilter.tsx
@@ -2,10 +2,10 @@ import { useState } from 'react';
 import { classNames } from '../../utils/styles';
 import AddIcon from './AddIcon';
 import styles from './MapFilter.module.css';
-import { mapFiltersCategories, MapFiltersCategory } from './mapFilters';
 import MapIcon from './MapIcon';
 import MarkerIcon from './MarkerIcon';
 import MarkersView from './MarkersView';
+import MenuOpenIcon from './MenuOpenIcon';
 import SearchIcon from './SearchIcon';
 
 type View = 'markers' | 'areas' | 'search';
@@ -21,33 +21,44 @@ function MapFilter({
   onNewFilterClick,
 }: MapFilterProps): JSX.Element {
   const [view, setView] = useState<View>('markers');
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleViewClick(view: View) {
+    setView(view);
+    setIsOpen(true);
+  }
 
   return (
-    <aside className={styles.container}>
-      {view === 'markers' && (
-        <MarkersView filters={filters} onFiltersChange={onFiltersChange} />
-      )}
+    <aside className={classNames(styles.container, isOpen && styles.open)}>
+      <div className={styles.content}>
+        {view === 'markers' && (
+          <MarkersView filters={filters} onFiltersChange={onFiltersChange} />
+        )}
+      </div>
       <nav className={styles.nav}>
         <button
           className={classNames(view === 'markers' && styles.nav__active)}
-          onClick={() => setView('markers')}
+          onClick={() => handleViewClick('markers')}
         >
           <MarkerIcon />
         </button>
         <button
           className={classNames(view === 'areas' && styles.nav__active)}
-          onClick={() => setView('areas')}
+          onClick={() => handleViewClick('areas')}
         >
           <MapIcon />
         </button>
         <button
           className={classNames(view === 'search' && styles.nav__active)}
-          onClick={() => setView('search')}
+          onClick={() => handleViewClick('search')}
         >
           <SearchIcon />
         </button>
         <button onClick={onNewFilterClick}>
           <AddIcon />
+        </button>
+        <button onClick={() => setIsOpen(!isOpen)}>
+          <MenuOpenIcon />
         </button>
       </nav>
     </aside>
