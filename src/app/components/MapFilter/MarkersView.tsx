2777265e03f1fa7b96a37888fe19c57e8f062655
lmachens <leon@machens.koeln>
2021-08-30 19:49:45 +0200

    ✨ Add show/hide all

--- src/app/components/MapFilter/MarkersView.tsx
+++ src/app/components/MapFilter/MarkersView.tsx
@@ -1,6 +1,5 @@
 import styles from './MarkersView.module.css';
-import { mapFiltersCategories } from './mapFilters';
-import CategoryTitle from './CategoryTitle';
+import { mapFilters, mapFiltersCategories } from './mapFilters';
 import { useRouter, useURL } from '../Router/Router';
 import { useMemo } from 'react';
 import MarkerSection from './MarkerSection';
@@ -30,6 +29,28 @@ function MarkersView(): JSX.Element {
 
   return (
     <>
+      <div className={styles.actions}>
+        <button
+          className={styles.action}
+          onClick={() => {
+            router.search({
+              mapFilters: mapFilters.map((filter) => filter.type).join(','),
+            });
+          }}
+        >
+          Show all
+        </button>
+        <button
+          className={styles.action}
+          onClick={() => {
+            router.search({
+              mapFilters: '',
+            });
+          }}
+        >
+          Hide all
+        </button>
+      </div>
       <div className={styles.list}>
         {mapFiltersCategories.map((mapFilterCategory) => (
           <MarkerSection
