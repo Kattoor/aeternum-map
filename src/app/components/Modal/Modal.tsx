2a690f5ca8300c589a56c2740549ab91aa55b0cc
lmachens <leon@machens.koeln>
2021-09-07 11:54:17 +0200

    ✨ Close modal by pressing ESC key

--- src/app/components/Modal/Modal.tsx
+++ src/app/components/Modal/Modal.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from 'react';
+import { useEffect } from 'react';
 import CloseIcon from '../icons/CloseIcon';
 import styles from './Modal.module.css';
 
@@ -8,6 +9,18 @@ type ModalProps = {
   onClose: () => void;
 };
 function Modal({ children, title, onClose }: ModalProps): JSX.Element {
+  useEffect(() => {
+    function handleKeyDown(event: KeyboardEvent): void {
+      if (event.key === 'Escape') {
+        onClose();
+      }
+    }
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
   return (
     <section className={styles.backdrop} onClick={onClose}>
       <div
